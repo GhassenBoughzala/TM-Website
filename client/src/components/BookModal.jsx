e8c2fd04d6a0e03373450646bbc1057deb27af12
pmi <wordpresshassen@gmail.com>
2023-11-17 02:35:34 +0100

    default

--- client/src/components/BookModal.jsx
+++ client/src/components/BookModal.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-mixed-operators */
-import React, { Fragment, useState, useEffect } from "react";
-import { Button, Form, Select, Input, InputNumber, Carousel } from "antd";
+import React, { Fragment, useState } from "react";
+import { Button, Form, Select, Input, InputNumber } from "antd";
 import dayjs from "dayjs";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -9,29 +9,13 @@ import { Subscribe } from "../redux/subs/subsActions";
 import { RedoOutlined } from "@ant-design/icons";
 import { currencies } from "../helpers/Constants";
 import shortid from  "shortid";
-import { useTranslation } from "react-i18next";
-
 const { Option } = Select;
 const { TextArea } = Input;
 
 export const BookModal = ({ ...props }) => {
-  const { t } = useTranslation();
   const navTo = useNavigate();
   const currentObj = props.currentObj;
   const [type, setType] = useState("");
-  const [showLevelMessage, setShowLevelMessage] = useState(false);
-
-  // useEffect to check if everything is okay whenever props.msg changes
-  useEffect(() => {
-    // Assuming props.msg is a code where 0 means an error and 1 means success.
-    if (props.msg == 0) {
-      props.setOpenModal(false);
-      navTo("/subscription");
-    } else if (props.msg == 1) {
-      toast.success(t('SubsResult-1')); // Assuming 'SubsResult-1' is a translation key
-    }
-  }, [props.msg]);
-
   const handleFormSubmit = () => {
     form
       .validateFields()
@@ -46,8 +30,8 @@ export const BookModal = ({ ...props }) => {
           hours: values.hours,
           currency: values.currency,
         });
-        //props.setOpenModal(false);
-        //navTo("/subscription");
+        props.setOpenModal(false);
+        navTo("/subscription");
       })
       .catch((errorInfo) => {
         toast.warn("Check your fields !");
@@ -67,11 +51,6 @@ export const BookModal = ({ ...props }) => {
     });
   };
 
-  const levelChange = (value) => {
-    // Check if the selected level is not "Beginner"
-    setShowLevelMessage(value !== "Beginner");
-  };
-
   const [form] = Form.useForm();
   const reset = () => {
     form.resetFields();
@@ -79,193 +58,168 @@ export const BookModal = ({ ...props }) => {
   };
 
   return (
-    <Carousel speed={1500} slidesToShow={1} dots={true}>
-      <div>
-      <Form
-        form={form}
-        className="form"
-        name="basic"
-        layout="vertical"
-        size={"medium"}
-        labelCol={{ span: 20 }}
-        autoComplete="off"
-      >
-        <div className="row">
-          <div className="form-outline text-start">
-            <div className="mb-4">
-              
-              
-              {props.msg && (
-                <div className="row">
-                  <p className="error-message">{props.msg}</p>
-                </div>
-              )}
-              <Form.Item
-                label="Select your level !!"
-                name="level"
-                className="mb-0"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please select your level",
-                  },
-                ]}
-              >
-                <Select options={options} onChange={levelChange} />
-              </Form.Item>
-              {showLevelMessage && (
-                <p className="msg_test_level">{t("msg_test_level")}</p>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="row">
+    <Form
+      form={form}
+      className="form"
+      name="basic"
+      layout="vertical"
+      size={"medium"}
+      labelCol={{ span: 20 }}
+      autoComplete="off"
+    >
+      <div className="row">
+        <div className="form-outline text-start">
           <Form.Item
-            label="Tell us more about your level :"
-            name="notes"
+            label="Select your level"
+            name="level"
             rules={[
               {
                 required: true,
-                message: "This field is required",
+                message: "Please select your level",
               },
             ]}
           >
-            <TextArea rows={2} />
+            <Select options={options} />
           </Form.Item>
         </div>
-        <div className="row">
-          <Form.Item
-            label="Pick an option"
-            name="type"
-            rules={[
-              {
-                required: type === "" && true,
-                message: "This field is required",
-              },
-            ]}
-          >
-            <div className="row text-start">
-              <div className="col-lg-4 col-xs-12 my-1 text-center">
-                <Button
-                  type={`${type === "Evening" ? "primary" : "default"}`}
-                  onClick={() => setType("Evening")}
-                >
-                  Next Evening Class
-                </Button>
-              </div>
-              <div className="col-lg-4 col-xs-12 my-1 text-center">
-                <Button
-                  type={`${type === "Private" ? "primary" : "default"}`}
-                  onClick={() => setType("Private")}
-                >
-                  Private Classes
-                </Button>
-              </div>
-              <div className="col-lg-4 col-xs-12 my-1 text-center">
-                <Button
-                  type={`${type === "Intensive" ? "primary" : "default"}`}
-                  onClick={() => setType("Intensive")}
-                >
-                  Intensive Classes
-                </Button>
-              </div>
-            </div>
-          </Form.Item>
-        </div>
-        {type === "Intensive" && (
-          <div className="row">
-            <div className="form-outline text-start">
-              {currentObj.sessions.length !== 0 && (
-                <Form.Item
-                  label="Select your sessions"
-                  name="sessions"
-                  onChange={handleChange}
-                >
-                  <Select mode="multiple" maxLength={3}>
-                    {currentObj.sessions.map((s, index) => {
-                      return (
-                        <Fragment key={shortid.generate() + index}>
-                          <Option
-                            value={s.startDate}
-                            key={s.id}
-                            label={`${dayjs(s.startDate).format("MMMM")} session`}
-                          >
-                            <p className=" text-dark">
-                              <b className="mx-1">
-                              {dayjs(s.startDate).format("MMM D")}
-                              </b>
-                              <b>- {dayjs(s.endDate).format("MMM D YYYY")}</b>
-                            </p>
-                          </Option>
-                        </Fragment>
-                      );
-                    })}
-                  </Select>
-                </Form.Item>
-              )}
-            </div>
-          </div>
-        )}
-
-        <div className="row">
-          <div className=" col col-6">
-            <Form.Item
-              label="Payment currency"
-              name="currency"
-              rules={[
-                {
-                  required: true,
-                  message: "Currency is required",
-                },
-              ]}
-            >
-              <Select options={currencies} />
-            </Form.Item>
-          </div>
-          <div className=" col col-6">
-            {type === "Private" && (
-              <Form.Item
-                label="Number of hours"
-                name="hours"
-                style={{width: "100%"}}
-                rules={[
-                  {
-                    required: type === "Private" && true,
-                    message: "This field is required",
-                  },
-                ]}
+      </div>
+      <div className="row">
+        <Form.Item
+          label="Tell us more about your level :"
+          name="notes"
+          rules={[
+            {
+              required: true,
+              message: "This field is required",
+            },
+          ]}
+        >
+          <TextArea rows={2} />
+        </Form.Item>
+      </div>
+      <div className="row">
+        <Form.Item
+          label="Pick an option"
+          name="type"
+          rules={[
+            {
+              required: type === "" && true,
+              message: "This field is required",
+            },
+          ]}
+        >
+          <div className="row text-start">
+            <div className="col-lg-4 col-xs-12 my-1 text-center">
+              <Button
+                type={`${type === "Evening" ? "primary" : "default"}`}
+                onClick={() => setType("Evening")}
               >
-                <InputNumber width={"100%"} />
+                Next Evening Class
+              </Button>
+            </div>
+            <div className="col-lg-4 col-xs-12 my-1 text-center">
+              <Button
+                type={`${type === "Private" ? "primary" : "default"}`}
+                onClick={() => setType("Private")}
+              >
+                Private Classes
+              </Button>
+            </div>
+            <div className="col-lg-4 col-xs-12 my-1 text-center">
+              <Button
+                type={`${type === "Intensive" ? "primary" : "default"}`}
+                onClick={() => setType("Intensive")}
+              >
+                Intensive Classes
+              </Button>
+            </div>
+          </div>
+        </Form.Item>
+      </div>
+      {type === "Intensive" && (
+        <div className="row">
+          <div className="form-outline text-start">
+            {currentObj.sessions.length !== 0 && (
+              <Form.Item
+                label="Select your sessions"
+                name="sessions"
+                onChange={handleChange}
+              >
+                <Select mode="multiple" maxLength={3}>
+                  {currentObj.sessions.map((s, index) => {
+                    return (
+                      <Fragment key={shortid.generate() + index}>
+                        <Option
+                          value={s.startDate}
+                          key={s.id}
+                          label={`${dayjs(s.startDate).format("MMMM")} session`}
+                        >
+                          <p className=" text-dark">
+                            <b className="mx-1">
+                            {dayjs(s.startDate).format("MMM D")}
+                            </b>
+                            <b>- {dayjs(s.endDate).format("MMM D YYYY")}</b>
+                          </p>
+                        </Option>
+                      </Fragment>
+                    );
+                  })}
+                </Select>
               </Form.Item>
             )}
           </div>
         </div>
-        <div className="form-outline text-center mt-1">
-          <Form.Item>
-            <Button
-              type="primary"
-              htmltype="submit"
-              onClick={() => {
-                handleFormSubmit();
-                handleEverythingOkay(); // Call the new event handler
-              }}
-            >
-              Confirm
-            </Button>
-            {type !== "" && (
-              <Button
-                type="dashed"
-                className="mx-2"
-                icon={<RedoOutlined />}
-                onClick={() => reset()}
-              ></Button>
-            )}
+      )}
+
+      <div className="row">
+        <div className=" col col-6">
+          <Form.Item
+            label="Payment currency"
+            name="currency"
+            rules={[
+              {
+                required: true,
+                message: "Currency is required",
+              },
+            ]}
+          >
+            <Select options={currencies} />
           </Form.Item>
         </div>
-      </Form>
+        <div className=" col col-6">
+          {type === "Private" && (
+            <Form.Item
+              label="Number of hours"
+              name="hours"
+              style={{width: "100%"}}
+              rules={[
+                {
+                  required: type === "Private" && true,
+                  message: "This field is required",
+                },
+              ]}
+            >
+              <InputNumber width={"100%"} />
+            </Form.Item>
+          )}
+        </div>
       </div>
-      <div>Test</div>
-    </Carousel>
+      <div className="form-outline text-center mt-1">
+        <Form.Item>
+          <Button type="primary" htmltype="submit" onClick={handleFormSubmit}>
+            Confirm
+          </Button>
+          {type !== "" && (
+            <Button
+              type="dashed"
+              className="mx-2"
+              icon={<RedoOutlined />}
+              onClick={() => reset()}
+            ></Button>
+          )}
+        </Form.Item>
+      </div>
+    </Form>
   );
 };
 
